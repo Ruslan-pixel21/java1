984ebc35c8cfbc9f4003b1519a0a8d8344d7dbf1
Ruslan-pixel21 <ruslan.ruslan123.ruslan123456789@gmail.com>
2024-09-06 04:24:02 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int egg = 4;
+        if (egg > 3)
+            System.out.println("Рекомендую приготовить омлет");
+        else
+            System.out.println("Рекомендую позавтракать бутербродами");
+
+        System.out.print("Приятного аппетита!");
+
+    }
+}
